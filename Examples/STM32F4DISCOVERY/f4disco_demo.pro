ef0e1844de08ea6af8b422f475456adcf7bbc7fc
andreili <admin@andreil.by>
2018-05-28 11:13:04 +0300

    Добавлен демонстрационный проект

---
+++ Examples/STM32F4DISCOVERY/f4disco_demo.pro
@@ -0,0 +1,94 @@
+QT -= core gui opengl
+
+APP = f4disco_demo
+TARGET = f4disco_demo
+
+CONFIG += c++11
+DEVICE = STM32F407xx
+
+QMAKE_LFLAGS += -T$$PWD/../../cortex-m4/stm32f407xx.ld
+
+DEFINES += $${DEVICE}
+
+DESTDIR = ./
+#win32:EXT = .exe
+
+#LIBS += -L/usr/lib/gcc/arm-none-eabi/8.1.0/ -lstdc++ -lc
+
+#QMAKE_CXXFLAGS += -O0
+#win32:QMAKE_LFLAGS += -L/mingw32/lib/gcc/arm-none-eabi/5.4.1/
+linux:QMAKE_LFLAGS += -L/usr/lib/gcc/arm-none-eabi/7.2.0/
+
+createlist.target = all
+createlist.commands += arm-none-eabi-objdump -S $${OUT_PWD}/$${APP}$${EXT} > $$join(APP,,,".lst")
+createlist.commands += && arm-none-eabi-objcopy -Obinary $${OUT_PWD}/$${APP}$${EXT} $${OUT_PWD}/$${APP}.bin
+!win32:createlist.commands += && arm-none-eabi-nm -nalS --size-sort $${OUT_PWD}/$${APP}$${EXT} | grep " T " | tail
+createlist.commands += && arm-none-eabi-size $${OUT_PWD}/$${APP}$${EXT}
+QMAKE_EXTRA_TARGETS += createlist
+
+INCLUDEPATH += ./inc/ ../../Drivers/CMSIS/Devices/ ../../Drivers/CMSIS/inc/ ../../inc/ ../../StdLib/inc/
+INCLUDEPATH += ../../Drivers/FATfs/inc/ ../../Third_Party/FatFs/ ../../Drivers/USB/inc/
+
+SOURCES += \
+    src/main.cpp \
+    ../../Drivers/CMSIS/Devices/ISRstm32f407xx.cpp \
+    ../../src/init.cpp \
+    ../../src/stm32_flash.cpp \
+    ../../src/stm32_gpio.cpp \
+    ../../src/stm32_nvic.cpp \
+    ../../src/stm32_pwr.cpp \
+    ../../src/stm32_rcc.cpp \
+    ../../src/stm32_rtc.cpp \
+    ../../src/stm32_sd.cpp \
+    ../../src/stm32_sdram.cpp \
+    ../../src/stm32_spi.cpp \
+    ../../src/stm32_systick.cpp \
+    ../../src/stm32_uart.cpp \
+    ../../StdLib/src/my_func.cpp \
+    ../../StdLib/src/xprintf.cpp \
+    ../../src/stm32_dma.cpp \
+    ../../Drivers/FATfs/src/fatfs.cpp \
+    ../../Drivers/FATfs/src/sddriver.cpp \
+    ../../Third_Party/FatFs/ff.c \
+    ../../Third_Party/FatFs/option/unicode.c \
+    ../../Third_Party/FatFs/option/ccsbcs.c \
+    ../../Third_Party/FatFs/option/syscall.c \
+    ../../Drivers/USB/src/usbhcore.cpp \
+    ../../src/stm32_hcd.cpp
+
+HEADERS += \
+    inc/stm32_conf.h \
+    ../../Drivers/CMSIS/Devices/ISRstm32f407xx.h \
+    ../../Drivers/CMSIS/Devices/stm32f407xx.h \
+    ../../Drivers/CMSIS/Devices/stm32f4xx.h \
+    ../../inc/init.h \
+    ../../inc/stm32_flash.h \
+    ../../inc/stm32_gpio.h \
+    ../../inc/stm32_inc.h \
+    ../../inc/stm32_nvic.h \
+    ../../inc/stm32_pwr.h \
+    ../../inc/stm32_rcc.h \
+    ../../inc/stm32_rtc.h \
+    ../../inc/stm32_sd.h \
+    ../../inc/stm32_sdram.h \
+    ../../inc/stm32_spi.h \
+    ../../inc/stm32_systick.h \
+    ../../inc/stm32_uart.h \
+    ../../StdLib/inc/bitbanding.h \
+    ../../StdLib/inc/my_func.h \
+    ../../StdLib/inc/xprintf.h \
+    ../../inc/stm32_def.h \
+    ../../inc/stm32_dma.h \
+    ../../Drivers/FATfs/inc/diskio.h \
+    ../../Drivers/FATfs/inc/diskiodriver.h \
+    ../../Drivers/FATfs/inc/fatfs.h \
+    ../../Drivers/FATfs/inc/ffconf.h \
+    ../../Drivers/FATfs/inc/sddriver.h \
+    ../../Third_Party/FatFs/ff.h \
+    ../../Third_Party/FatFs/integer.h \
+    ../../Drivers/USB/inc/usbhcore.h \
+    inc/usbh_config.h \
+    ../../Drivers/USB/inc/usbh_class.h \
+    ../../inc/stm32_hcd.h
+
+
---
+++ Examples/STM32F4DISCOVERY/src/main.cpp
@@ -0,0 +1,129 @@
+#include "stm32_inc.h"
+#include "my_func.h"
+#include "xprintf.h"
+#include "usbhcore.h"
+
+#define LED_PORT gpiod
+#define LED3_PIN GPIO_PIN_13
+#define LED4_PIN GPIO_PIN_12
+#define LED5_PIN GPIO_PIN_14
+#define LED6_PIN GPIO_PIN_15
+
+#define SYS_PULSE_PORT gpiod
+#define SYS_PULSE_PIN  GPIO_PIN_1
+
+#define BTN_PORT gpioa
+#define BTN_PIN  GPIO_PIN_0
+
+void xfunc_out(unsigned char c)
+{
+    uart6.send_char(c);
+}
+
+uint32_t    val_i = 0;
+
+void usb_fs_proc(USBHCore* core, EHostUser reason)
+{
+    UNUSED(core);
+    switch (reason)
+    {
+    case EHostUser::SELECT_CONFIGURATION:
+        debug_out("Select configuration\n\r");
+        break;
+    case EHostUser::CLASS_ACTIVE:
+        debug_out("Class active\n\r");
+        break;
+    case EHostUser::CLASS_SELECTED:
+        debug_out("Class selected\n\r");
+        break;
+    case EHostUser::CONNECTION:
+        debug_out("Connection\n\r");
+        break;
+    case EHostUser::DISCONNECTION:
+        debug_out("Disconnection\n\r");
+        break;
+    case EHostUser::UNRECOVERED_ERROR:
+        debug_out("Unrecovered error\n\r");
+        break;
+    }
+}
+
+int main()
+{
+    STM32_RCC::enable_clk_GPIOA();
+    STM32_RCC::enable_clk_GPIOD();
+
+    LED_PORT.set_config(LED3_PIN | LED4_PIN | LED5_PIN | LED6_PIN, GPIO_MODE_OUTPUT_PP, 0, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL);
+    gpioa.pin_OFF(LED3_PIN);
+
+    SYS_PULSE_PORT.set_config(SYS_PULSE_PIN, GPIO_MODE_OUTPUT_PP, 0, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL);
+    BTN_PORT.set_config(BTN_PIN, GPIO_MODE_INPUT, 0, GPIO_SPEED_FREQ_LOW, GPIO_PULLDOWN);
+
+    STM32_USB_PWR_FS_PORT.set_config(STM32_USB_PWR_FS_PIN, GPIO_MODE_OUTPUT_PP, 0, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL);
+
+    uart6.init(STM32_BRATE_UART6);
+
+    uart6.send_str("\n\rSTM32F4DISCOVERY demo project"
+                   "\n\r\tBased on C++ frameork\n\r", TXRX_MODE::INTERRUPT);
+
+    usb_FS.init(usb_fs_proc, HOST_FS);
+
+    uint32_t ticks;
+    while (1)
+    {
+        SYS_PULSE_PORT.pin_ON(GPIO_PIN_1);
+        ticks = STM32_SYSTICK::get_tick();
+
+        if ((ticks & 0x1ff) == 0x1ff)
+        {
+            xprintf("ticks=%d; iteration=%d\r", ticks, val_i++);
+            LED_PORT.pin_toggle(LED5_PIN);
+        }
+
+        if (BTN_PORT.pin_read(BTN_PIN))
+            LED_PORT.pin_ON(LED4_PIN);
+        else
+            LED_PORT.pin_OFF(LED4_PIN);
+
+        SYS_PULSE_PORT.pin_OFF(GPIO_PIN_1);
+
+        STM32_SYSTICK::delay_to(ticks + 1);
+
+        usb_FS.process();
+    }
+    return 0;
+}
+
+void Error_Handler()
+{
+    while (1)
+        gpioa.pin_ON(LED6_PIN);
+}
+
+void disconnect_callback(STM32_HCD *hcd)
+{
+    ((USBHCore*)hcd->get_data())->LL_disconnect();
+    debug_out("USB disconnected\n\r");
+}
+
+void connect_callback(STM32_HCD *hcd)
+{
+    ((USBHCore*)hcd->get_data())->LL_connect();
+    debug_out("USB connected\n\r");
+}
+
+void SOF_callback(STM32_HCD *hcd)
+{
+    ((USBHCore*)hcd->get_data())->LL_inc_timer();
+}
+
+void HC_notify_URB_change_callback(STM32_HCD *hcd, uint8_t ch_num, EURBState urb_state)
+{
+#if (USBH_USE_OS == 1)
+    ((USBHCore*)hcd->get_data())->LL_notify_URB_change();
+#else
+    UNUSED(hcd);
+#endif
+    UNUSED(ch_num);
+    UNUSED(urb_state);
+}
